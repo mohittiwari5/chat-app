a0f113c8264255d66f0e8d63a422f646ebdeaeae
Mohit Tiwari <sachin@bodacious-server.com>
2023-03-02 13:26:01 +0530

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by SACHIN GAHLAWAT on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
